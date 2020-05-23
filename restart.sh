754de0545188460e71c22251bcb4e6cf6003c9dd
Havlong <grishin_evgenii@inbox.ru>
2020-05-23 09:51:21 +0300

    restart.sh edited

--- restart.sh
+++ restart.sh
@@ -1,4 +1,5 @@
 docker container stop pixeldata_app
 docker container rm pixeldata_app
+git pull
 docker build -t pixeldata:latest .
 docker run -dit -p 5000:80 --name pixeldata_app --restart unless-stopped pixeldata
