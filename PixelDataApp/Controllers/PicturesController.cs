23692d745e691e8be50378c6cbecbc8a29ff4811
Havlong <grishin_evgenii@inbox.ru>
2020-05-24 12:12:05 +0300

    Removed token validation on image uploading

--- PixelDataApp/Controllers/PicturesController.cs
+++ PixelDataApp/Controllers/PicturesController.cs
@@ -55,10 +55,7 @@ namespace PixelDataApp.Controllers
         }
 
         // POST: Pictures/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Info,AnswerId")] Picture picture, IFormFile file)
         {
             if (ModelState.IsValid)
