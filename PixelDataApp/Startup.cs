eeae2cd7b1f383399186d23131cba599bf58b65d
Havlong <grishin_evgenii@inbox.ru>
2020-05-25 03:02:22 +0300

    Imports in configuration file added

--- PixelDataApp/Startup.cs
+++ PixelDataApp/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.StaticFiles.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.EntityFrameworkCore;
 using PixelDataApp.Data;
 
